80047953b50f797b5e6c55efda6ce16fd8a66452
zee-german <franz.lutzenberger@gmail.com>
2016-04-18 09:26:32 +0100

    secured publish and subscription behaviour

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,18 @@ import { check } from 'meteor/check'
 
 export const Tasks = new Mongo.Collection('tasks')
 
+if ( Meteor.isServer ) {
+  // Only publish tasks that are public or belong  to the current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId }
+      ]
+    })
+  })
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String)
@@ -24,6 +36,12 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String)
 
+    const task = Tasks.findOne(taskId)
+    if ( task.private && task.owner !== Meteor.userId() ) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized')
+    }
+
     Tasks.remove(taskId)
   },
 
@@ -31,6 +49,26 @@ Meteor.methods({
     check(taskId, String)
     check(setChecked, Boolean)
 
+    const task = Tasks.findOne(taskId)
+    if ( task.private && task.owner !== Meteor.userId() ) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized')
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked } })
+  },
+
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String)
+    check(setToPrivate, Boolean)
+
+    const task = Tasks.findOne(taskId)
+
+    // make sure only the task owner can make a task private
+    if ( task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized')
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } })
   }
 })
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { Meteor } from 'meteor/meteor'
+import classnames from 'classnames'
 
 import { Tasks } from '../api/tasks'
 
@@ -14,10 +15,17 @@ export default class Task extends Component {
     Meteor.call('tasks.remove', this.props.task._id)
   }
 
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private)
+  }
+
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : ''
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private
+    })
 
     return (
       <li className={taskClassName}>
@@ -32,6 +40,12 @@ export default class Task extends Component {
           onClick={this.toggleChecked.bind(this)}
         />
 
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public'}
+          </button>
+        ) : ''}
+
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}
         </span>
@@ -43,5 +57,6 @@ export default class Task extends Component {
 Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is requried
-  task: PropTypes.object.isRequired
+  task: PropTypes.object.isRequired,
+  showPrivateButton: React.PropTypes.bool.isRequired
 }
